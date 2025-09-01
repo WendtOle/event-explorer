8db0d538dade12aa293a62fb4e9fdd53ca6100c3
Ole <ole@Mac.fritz.box>
2025-09-01 07:23:19 +0200

    Remove nextjs workarounds

--- src/app/Map.tsx
+++ src/app/Map.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
-import type { LatLngBounds, LatLngTuple } from 'leaflet';
-import { Event, WayPoint } from "./useEvents";
+import L, { type LatLngBounds, type LatLngTuple } from 'leaflet';
+import 'leaflet.markercluster';
 
 interface MapComponentProps {
 	event?: Event;
@@ -25,27 +25,26 @@ const MapComponent = ({ event, events, onMapChange, disableFlyTo, onEventClick }
 		if (!eventLayer) {
 			return
 		}
-		import('leaflet').then(L => {
-			const addMarker = (currEvent: Event) => {
-				if (!!event && event.id === currEvent.id) {
-					return
-				}
-				const point = currEvent.way_points[0]
-				if (!point) {
-					return
-				}
-
-				L.marker(point.position, {
-					icon: L.divIcon({
-						className: 'my-div-icon',
-						html: '<span class="material-icons" style="color: #cc6600;">emoji_people</span>',
-						iconSize: [32, 32],
-						iconAnchor: [16, 22],
-					})
-				}).bindTooltip(currEvent.thema.slice(0, 40) + " ...").addTo(eventLayer).on("click", () => onEventClick(currEvent))
+		
+		const addMarker = (currEvent: Event) => {
+			if (!!event && event.id === currEvent.id) {
+				return
 			}
-			events.forEach(addMarker)
-		})
+			const point = currEvent.way_points[0]
+			if (!point) {
+				return
+			}
+
+			L.marker(point.position, {
+				icon: L.divIcon({
+					className: 'my-div-icon',
+					html: '<span class="material-icons" style="color: #cc6600;">emoji_people</span>',
+					iconSize: [32, 32],
+					iconAnchor: [16, 22],
+				})
+			}).bindTooltip(currEvent.thema.slice(0, 40) + " ...").addTo(eventLayer).on("click", () => onEventClick(currEvent))
+		}
+		events.forEach(addMarker)
 	}, [setEventLayer, events, event, map])
 
 	useEffect(() => {
@@ -54,73 +53,68 @@ const MapComponent = ({ event, events, onMapChange, disableFlyTo, onEventClick }
 			return
 		}
 		const { way_points, bounds } = event;
-		import('leaflet').then(L => {
-			const addMarker = (point: WayPoint, markerIcon: 'flag' | 'sports_score') => {
-				L.marker(point.position, {
-					icon: L.divIcon({
-						className: 'my-div-icon selected-event-marker',
-						html: `<span class="material-icons" style="color: #b266ff;">${markerIcon}</span>`,
-						iconSize: [32, 32],
-						iconAnchor: [16, 22],
-					})
-				}).bindTooltip(point.text).addTo(selectedMarkerLayer)
-			}
+		
+		const addMarker = (point: WayPoint, markerIcon: 'flag' | 'sports_score') => {
+			L.marker(point.position, {
+				icon: L.divIcon({
+					className: 'my-div-icon selected-event-marker',
+					html: `<span class="material-icons" style="color: #b266ff;">${markerIcon}</span>`,
+					iconSize: [32, 32],
+					iconAnchor: [16, 22],
+				})
+			}).bindTooltip(point.text).addTo(selectedMarkerLayer)
+		}
 
-			if (way_points.length === 1) {
-				addMarker(way_points[0], 'flag')
-			} else {
-				L.polyline(way_points.map(({ position }) => position), {
-					color: colors[0],
-				}).addTo(selectedMarkerLayer);
-				way_points.forEach((point, i, all) => {
-					const isSameAsPrevious = i !== 0 && point.text === all[i - 1].text
-					if (isSameAsPrevious) {
-						return
-					}
-					const isSameAsFirst = i > 0 && point.text === all[0].text
-					if (isSameAsFirst) {
-						return
-					}
-					addMarker(point, i === 0 ? 'sports_score' : 'flag')
+		if (way_points.length === 1) {
+			addMarker(way_points[0], 'flag')
+		} else {
+			L.polyline(way_points.map(({ position }) => position), {
+				color: colors[0],
+			}).addTo(selectedMarkerLayer);
+			way_points.forEach((point, i, all) => {
+				const isSameAsPrevious = i !== 0 && point.text === all[i - 1].text
+				if (isSameAsPrevious) {
+					return
 				}
-				)
+				const isSameAsFirst = i > 0 && point.text === all[0].text
+				if (isSameAsFirst) {
+					return
+				}
+				addMarker(point, i === 0 ? 'sports_score' : 'flag')
 			}
-			if (disableFlyTo) {
-				return
-			}
-			if (way_points.length === 1) {
-				map?.flyTo(way_points[0].position, 13)
-			}
-			if (way_points.length > 1 && bounds !== undefined) {
-				map?.flyToBounds(bounds)
-			}
-		})
+			)
+		}
+		if (disableFlyTo) {
+			return
+		}
+		if (way_points.length === 1) {
+			map?.flyTo(way_points[0].position, 13)
+		}
+		if (way_points.length > 1 && bounds !== undefined) {
+			map?.flyToBounds(bounds)
+		}
 	}, [event, map, selectedMarkerLayer])
 
 	useEffect(() => {
-		import('leaflet').then(async () => {
-			await import('leaflet.markercluster')
-			const leaflet = window.L;
-			const map = leaflet.map('map').setView(berlinCenter, 13);
-			leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-				attribution: '&copy; OpenStreetMap contributors'
-			}).addTo(map);
-			const clusterGroup = leaflet.markerClusterGroup({
-				maxClusterRadius: 40,
-				showCoverageOnHover: false,
-				zoomToBoundsOnClick: true
-			});
-			setEventLayer(clusterGroup.addTo(map))
-			setSelectedMarkerLayer(leaflet.layerGroup().addTo(map));
-			setMap(map)
-			onMapChange?.(map.getBounds())
-			const onMoveEnd = () => onMapChange?.(map.getBounds())
-			map.on("moveend", onMoveEnd)
-			return () => {
-				map.off("moveend", onMoveEnd)
-				map.remove()
-			}
-		})
+		const map = L.map('map').setView(berlinCenter, 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; OpenStreetMap contributors'
+		}).addTo(map);
+		const clusterGroup = L.markerClusterGroup({
+			maxClusterRadius: 40,
+			showCoverageOnHover: false,
+			zoomToBoundsOnClick: true
+		});
+		setEventLayer(clusterGroup.addTo(map))
+		setSelectedMarkerLayer(L.layerGroup().addTo(map));
+		setMap(map)
+		onMapChange?.(map.getBounds())
+		const onMoveEnd = () => onMapChange?.(map.getBounds())
+		map.on("moveend", onMoveEnd)
+		return () => {
+			map.off("moveend", onMoveEnd)
+			map.remove()
+		}
 	}, []);
 	return (
 		<div className="w-full h-44 2">
